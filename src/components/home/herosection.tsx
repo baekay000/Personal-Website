965fc3cd03f1fe0419c9efa95f5c01445b457225
6u9 <6u9@ornl.gov>
2024-03-28 20:15:05 -0500

    hero section update

--- src/components/home/herosection.tsx
+++ src/components/home/herosection.tsx
@@ -12,39 +12,6 @@ const HeroSection = () => {
     }
   }
 
-  // // Smooth Scroll To : copied from ChatGPT on 3/28/24
-  // function smoothScrollTo(target: string, duration: number) {
-  //   const targetElement = document.querySelector(target);
-  //   const targetPosition =
-  //     targetElement.getBoundingClientRect().top + window.pageYOffset;
-  //   const startPosition = window.pageYOffset;
-  //   const distance = targetPosition - startPosition;
-  //   let startTime: number | null = null;
-
-  //   function animation(currentTime: number) {
-  //     if (startTime === null) startTime = currentTime;
-  //     const timeElapsed = currentTime - startTime;
-  //     const ease = easeInOutQuad(
-  //       timeElapsed,
-  //       startPosition,
-  //       distance,
-  //       duration
-  //     );
-  //     window.scrollTo(0, ease);
-  //     if (timeElapsed < duration) requestAnimationFrame(animation);
-  //   }
-
-  //   function easeInOutQuad(t: number, b: number, c: number, d: number) {
-  //     t /= d / 2;
-  //     if (t < 1) return (c / 2) * t * t + b;
-  //     t--;
-  //     return (-c / 2) * (t * (t - 2) - 1) + b;
-  //   }
-
-  //   requestAnimationFrame(animation);
-  // }
-
-
   return (
     <div>
       <img 
