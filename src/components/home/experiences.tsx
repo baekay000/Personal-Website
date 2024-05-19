dabd5562e669295bafc613c458e771f8a57a4e4d
6u9 <6u9@ornl.gov>
2024-05-19 17:18:00 -0500

    test

--- src/components/home/experiences.tsx
+++ src/components/home/experiences.tsx
@@ -49,6 +49,7 @@ function Experiences() {
                 </p>
               </div>
             </div>
+            {/* flicking is not working */}
             {/* <div className="text-slate-400 text-2xl px-6 py-4 rounded-3xl bg-zinc-300 mr-1 my-4 hover:bg-orange-400 hover:text-slate-950">
               <div className="">
                 URSI Computer Vision Internship
